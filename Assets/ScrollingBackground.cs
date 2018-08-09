0a72a27e9c1572fec4bea70b7f06ef94e0b68931
maxbrian@ymail.com <Eclipse256>
2018-08-08 19:02:40 -0700

    Initial commit

---
+++ Assets/ScrollingBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+	[SerializeField] float speeds = 10f;
+	// Update is called once per frame
+	void Update () 
+	{
+		MeshRenderer MR = GetComponent<MeshRenderer> ();
+		Material mate = MR.material;
+		Vector2 offset = mate.mainTextureOffset;
+		if (offset.y >= 10) 
+		{
+			offset.y = 0;
+		}
+		offset.y += Time.deltaTime / speeds;
+		mate.mainTextureOffset = offset;
+
+		
+	}
+}
---
+++ Assets/playermovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+	[SerializeField] float speed = 10f;
+	[SerializeField] KeyCode upkey;
+	[SerializeField] KeyCode DownKey;
+	[SerializeField] KeyCode RightKey;
+	[SerializeField] KeyCode LeftKey;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (upkey)) 
+		{
+			this.transform.Translate (new Vector3 (0f, speed, 0f)* Time.deltaTime );
+		}
+		if (Input.GetKey (DownKey)) 
+		{
+			this.transform.Translate (new Vector3 (0f, -speed, 0f) * Time.deltaTime);
+		}
+		if (Input.GetKey (RightKey)) 
+		{
+			this.transform.Translate (new Vector3 (speed,0f, 0f)* Time.deltaTime );
+		}
+		if (Input.GetKey (LeftKey)) 
+		{
+			this.transform.Translate (new Vector3 (-speed,0f, 0f) * Time.deltaTime);
+		}
+				
+			
+	}
+}
